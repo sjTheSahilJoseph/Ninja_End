f473bad5d5f49aa95e59e7fcf51527cb593c8946
sjTheSahilJoseph <sjthesahiljoseph@gmail.com>
2024-05-31 04:16:15 +0500

    Added Data and Created Object of game

--- game.py
+++ game.py
@@ -2,20 +2,24 @@
 import sys
 import pygame
 
-pygame.init()
+class Game:
+    def __init__(self):
+        pygame.init()
+        pygame.display.set_caption("Ninja End - SJ the Sahil Joseph")
 
+        self.screen = pygame.display.set_mode((640, 480))
+        self.clock = pygame.time.Clock()
 
-screen = pygame.display.set_mode((640, 480))
-
-clock = pygame.time.Clock()
-
-while True:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            sys.exit()
-            
-    pygame.display.update()
-    clock.tick(60)
+    def run(self):
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+                    
+            pygame.display.update()
+            self.clock.tick(60)
 
     
+        
+Game().run()
