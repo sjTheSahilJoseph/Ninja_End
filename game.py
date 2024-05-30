74fac2fe0b4e1a6dc7b7a678a94701135cf6df62
sjTheSahilJoseph <sjthesahiljoseph@gmail.com>
2024-05-31 04:18:44 +0500

    Added cloud image

--- game.py
+++ game.py
@@ -10,8 +10,12 @@ class Game:
         self.screen = pygame.display.set_mode((640, 480))
         self.clock = pygame.time.Clock()
 
+        self.img = pygame.image.load("data/images/clouds/cloud_1.png")
+
     def run(self):
         while True:
+            self.screen.blit(self.img, (100, 200))
+            
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     pygame.quit()
