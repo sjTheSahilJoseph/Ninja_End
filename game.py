5fd619c00218321aa995c076df681a04ea786453
sjTheSahilJoseph <sjthesahiljoseph@gmail.com>
2024-06-01 01:24:39 +0500

    image loaded for background

--- game.py
+++ game.py
@@ -12,8 +12,8 @@ class Game:
         pygame.init()
         pygame.display.set_caption("Ninja End - SJ Joseph")
 
-        self.width = 1080
-        self.height = 720
+        self.width = 640
+        self.height = 480
 
         self.screen = pygame.display.set_mode((self.width, self.height))
         self.display = pygame.Surface((self.width/2, self.height/2))
@@ -25,7 +25,8 @@ class Game:
             "grass": load_images("tiles/grass"),
             "large_decor": load_images("tiles/large_decor"),
             "stone": load_images("tiles/stone"),
-            "player": load_image("entities/player.png")
+            "player": load_image("entities/player.png"),
+            "background": load_image("background.png")
         }
         
         self.player = PhysicsEntity(self, 'player', (50, 50), (8, 15))
@@ -38,15 +39,17 @@ class Game:
     def run(self):
         while True:
             
-            self.display.fill((15, 200, 255))
+            self.display.blit(self.assets["background"], (0, 0))
 
             self.scroll[0] += (self.player.rect().centerx - self.display.get_width() / 2 - self.scroll[0]) / 30
             self.scroll[1] += (self.player.rect().centery - self.display.get_height() / 2 - self.scroll[1]) / 30
+
+            render_scroll = (int(self.scroll[0]), int(self.scroll[1]))
             
-            self.tilemap.render(self.display, offset = self.scroll)
+            self.tilemap.render(self.display, offset = render_scroll)
 
             self.player.update(self.tilemap, (self.movement[1] - self.movement[0], 0))
-            self.player.render(self.display, offset = self.scroll)
+            self.player.render(self.display, offset = render_scroll)
 
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
