b7f54a3bee0ec088c0640cf4b2ec3e96d428411a
sjTheSahilJoseph <sjthesahiljoseph@gmail.com>
2024-06-01 01:18:54 +0500

    Camera working but needs to fix some stuff

--- scripts/tilemap.py
+++ scripts/tilemap.py
@@ -35,12 +35,12 @@ class Tilemap:
                 rects.append(pygame.Rect(tile["pos"][0] * self.tile_size, tile["pos"][1] * self.tile_size, self.tile_size, self.tile_size))
         return rects
 
-    def render(self, surf):
+    def render(self, surf, offset = (0, 0)):
         for tile in self.offgrid_tiles:
-            surf.blit(self.game.assets[tile["type"]][tile["variant"]], (tile["pos"]))
+            surf.blit(self.game.assets[tile["type"]][tile["variant"]], (tile["pos"][0] - offset[0], tile["pos"][1] - offset[1]))
             
         for location in self.tile_map:
             tile = self.tile_map[location]
-            surf.blit(self.game.assets[tile["type"]][tile["variant"]], (tile["pos"][0] * self.tile_size, tile["pos"][1] * self.tile_size))
+            surf.blit(self.game.assets[tile["type"]][tile["variant"]], (tile["pos"][0] * self.tile_size - offset[0], tile["pos"][1] * self.tile_size - offset[1]))
 
                       
