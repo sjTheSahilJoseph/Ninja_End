4fbf1bf803367ebef307decd0068d7f3e53e1ea7
sjTheSahilJoseph <sjthesahiljoseph@gmail.com>
2024-06-03 01:42:27 +0500

    Removed for loop tiles

--- scripts/tilemap.py
+++ scripts/tilemap.py
@@ -11,10 +11,6 @@ class Tilemap:
         self.tile_map = {}
         self.offgrid_tiles = []
 
-        for i in range(10):
-            self.tile_map[str(3 + i) + ";10"] = {"type": "grass", "variant": 1, "pos": (3 + i, 10)}
-            self.tile_map["10;" + str(5 + i)] = {"type": "stone", "variant": 1, "pos": (10, 5 + i)}
-
     def tiles_around(self, pos):
         tiles = []
         tile_loc = (int(pos[0] // self.tile_size), int(pos[1] // self.tile_size))
